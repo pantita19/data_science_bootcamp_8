712846599e9307d7214658c9669502968cf75eca
Pantita Bupphakit <pantitabupphakit@MacBook-Pro-khxng-Pantita.local>
2023-12-04 00:03:45 +0700

    add new file

---
+++ data_viz.R
@@ -0,0 +1,4 @@
+# load ggplot library
+
+library(ggplot2)
+ggplot(diamonds, aes(carat, price))+geom_point()
--- helllo_world.r
+++ helllo_world.r
@@ -6,3 +6,8 @@ library(dplyr)
 mtcars %>%
   select(1:5) %>%
   filter(mpg > 30)
+
+library(dplyr)
+mtcars %>%
+  select(mpg) %>%
+  filter(mpg < 30)
